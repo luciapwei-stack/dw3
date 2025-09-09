4c13d61016d480a20929e1c31db45e3cab629a4d
Lucia <luciapwei@gmail.com>
2025-09-09 20:30:21 -0300

    inicia la historia

---
+++ codigo.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburgerButton = document.querySelector(".hamburger-button");
+  const mobileMenu = document.querySelector(".mobile-menu");
+
+  hamburgerButton.addEventListener("click", () => {
+    mobileMenu.classList.toggle("active");
+  });
+});
+
+//cuando se envie el form, el input con name "subject" tome el value del input con name "nombre"
+
+const formulario = document.querySelector("form[name=newsletter]");
+
+
+
+formulario.addEventListener("submit", cambioAsunto);
+
+function cambioAsunto() {
+  const asunto = document.querySelector("input[name=subject]");
+  const nombre = document.querySelector("input[name=nombre]");
+
+  asunto.value=`%{submissionId} - Consulta de ${nombre.value} en el formulario`
+}
